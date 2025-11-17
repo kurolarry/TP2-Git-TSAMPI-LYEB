1060233fb0346255ea0d30a14feeb91d8682c5ec
Lucien6002 <lyebl@3il.fr>
2025-11-17 16:17:31 +0100

    Ajout d'une deuxième ligne au message B

--- src/MessageB.java
+++ src/MessageB.java
@@ -2,7 +2,7 @@
 public class MessageB {
 	public static void main(String[] args) {
 		System.out.println("Mon message perso du membre B");
-		System.out.println("Mon 2e message perso du membre A");
+		System.out.println("Mon 2e message perso du membre B");
 	
 	}
 
